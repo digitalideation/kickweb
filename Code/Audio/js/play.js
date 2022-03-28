fa26dba08fe546847529296f4c525dfee3cb47d8
hzuellig <zuellig@internauta.ch>
2022-03-28 17:53:31 +0200

    Commit Audio Example

---
+++ Code/Audio/js/play.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hzuellig on 02.07.21.
+ */
+
+function playAudio() {
+    let audioel = document.querySelector("audio");
+    audioel.play();
+    document.querySelector('button').classList.toggle('playstate');
+
+
+}
